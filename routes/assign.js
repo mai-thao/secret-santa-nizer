b2b6c916d98418903a7d473f810b3902bb8d71ad
mai-thao <mthaominn@gmail.com>
2025-07-23 17:43:00 -0500

    Add logic to save output to a file for persistance

--- routes/assign.js
+++ routes/assign.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const db = require('../database/setup');
 
 const router = express.Router();
@@ -8,6 +9,13 @@ router.post('/assign', (req, res) => {
     const names = req.body.names;
     console.log(names);
     res.json(names);
+
+    const jsonString = JSON.stringify(names, null, 2);
+    const fileName = "names-backup.json";
+
+    const outputPath = path.join(__dirname, '../backups');
+    fs.writeFileSync(path.join(outputPath, fileName), jsonString);
+    console.log(`Data successfully saved to ${fileName}`);
 });
 
 module.exports = router;
