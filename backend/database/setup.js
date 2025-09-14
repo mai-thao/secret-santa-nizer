b94ae86664b4605845d31ce80e641101ac79a48f
mai-thao <mthaominn@gmail.com>
2025-09-14 18:46:51 -0500

    Comment out better-sqlite3 for now

--- backend/database/setup.js
+++ backend/database/setup.js
@@ -1,13 +1,13 @@
-// We are using better-sqlite3 because it is a simple serverless db that stores and persists all data into a .db file
-const Database = require('better-sqlite3');
-const db = new Database('secret-santa.db');
-
-db.prepare(`
-  CREATE TABLE IF NOT EXISTS assignments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    giver TEXT NOT NULL,
-    receiver TEXT NOT NULL
-  )
-`).run();
-
-module.exports = db; // Exports this db instance for reuse
+//// We are using better-sqlite3 because it is a simple serverless db that stores and persists all data into a .db file
+//const Database = require('better-sqlite3');
+//const db = new Database('secret-santa.db');
+//
+//db.prepare(`
+//  CREATE TABLE IF NOT EXISTS assignments (
+//    id INTEGER PRIMARY KEY AUTOINCREMENT,
+//    giver TEXT NOT NULL,
+//    receiver TEXT NOT NULL
+//  )
+//`).run();
+//
+//module.exports = db; // Exports this db instance for reuse
