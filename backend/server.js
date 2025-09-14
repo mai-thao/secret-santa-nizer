01b8807c0d07480ca904c6c38ef1390474d440c3
mai-thao <mthaominn@gmail.com>
2025-09-14 18:48:23 -0500

    Allow cross-origin communication between frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require("cors");
 const assignRoute = require('./routes/assign');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" })); // Allow Vite frontend cross-origin requests
 app.use('/api', assignRoute);
 
 app.listen(PORT, () => {
